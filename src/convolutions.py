7608e9414e1f593ec14825adc60bac404f736be9
Nate <78279182+natelharrison@users.noreply.github.com>
2026-01-29 12:45:43 -0800

    implemented sequential_convolve, tested against scipy implementations

--- src/convolutions.py
+++ src/convolutions.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Literal
 
 def naive_2d_convolve(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
     """
@@ -61,3 +62,38 @@ def numpy_2d_convolve(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
     image_convolution = np.einsum('ijab, ab->ij', image_views, kernel, optimize=True)
     return image_convolution.astype(image.dtype)
 
+
+def convolve_1d(image:np.ndarray, kernel:np.ndarray, axis:int, pad_mode:Literal['reflect']='reflect')->np.ndarray:
+    signal = np.moveaxis(image, axis, -1)
+
+    kernel_size = kernel.shape[0]
+    radius = kernel_size // 2
+
+    pad_width = [(0,0)] * signal.ndim
+    pad_width[-1] = (radius, radius)
+    padded_signal = np.pad(signal, pad_width, mode=pad_mode)
+
+    strides = padded_signal.strides
+    *base_shape, axis_length = signal.shape
+
+    windows = np.lib.stride_tricks.as_strided(
+        padded_signal,
+        (*base_shape, axis_length, kernel_size),
+        (*strides[:-1], strides[-1], strides[-1]),
+        writeable=False
+    )
+
+    # Not sure if I need to implement clipping to dtype min, max
+    # That would be a lot of work... (I should be but let's ignore it for now)
+    convolved = np.einsum('...lk,k->...l', windows, kernel)
+    return np.moveaxis(convolved, -1, axis)
+
+
+def sequential_convolve(image:np.ndarray, kernel:np.ndarray)->np.ndarray:
+    # Ideally I work with float64 and then would clip the output to image.dtype min/max and then cast to image.dtype
+    # But that seems tedious for this learning project so we return float32 result
+    working_image = image.astype(np.float32, copy=True)
+    kernel = kernel.astype(np.float32)
+    for ax in range(working_image.ndim):
+        working_image = convolve_1d(working_image, kernel, ax)
+    return working_image
